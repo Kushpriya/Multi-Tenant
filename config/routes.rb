4a76d5f130861ebe211cb432be23e3b49002f157
Kushpriya <priyakushawaha73@gmail.com>
2024-08-13 16:43:54 +0545

    validation updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations',sessions:'users/sessions' }
+
   get "home/index"
   get "home/dashboard"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -13,5 +14,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "home#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_13_050735) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_13_092112) do
   create_table "memberships", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "organization_id", null: false
@@ -34,6 +34,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_13_050735) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "organization_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
